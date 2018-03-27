185295da7d706ffd9e992e3d489a0fb46d941d37
nick-otter <nicholas.a.otter@gmail.com>
2018-03-27 12:38:01 +0100

    Post Deploy: updated move to touch in backload scripts

--- acl_backload_data_move.sh
+++ acl_backload_data_move.sh
@@ -5,6 +5,7 @@ chown wherescape:SSM /ADT/backload/acl/tmp/*.CSV
 cp /ADT/backload/acl/tmp/*.CSV /ADT/archive/acl
 for file in /ADT/backload/acl/tmp/*.CSV
 do
-  mv "$file" "${file%.CSV}.CSV.done"
+  touch "${file%.CSV}.CSV.done"
 done
 mv /ADT/backload/acl/tmp/*.CSV.done /ADT/data/acl
+mv /ADT/backload/acl/tmp/*.CSV /ADT/data/acl
