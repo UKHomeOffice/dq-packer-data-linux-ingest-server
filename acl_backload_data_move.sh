6f5ae3892c2df396ec50d983571d1f760122c19f
nick-otter <nicholas.a.otter@gmail.com>
2018-03-27 09:32:58 +0100

    Post Deploy: added .done in backload scripts

--- acl_backload_data_move.sh
+++ acl_backload_data_move.sh
@@ -3,4 +3,8 @@
 mv /ADT/backload/acl/*.CSV /ADT/backload/acl/tmp
 chown wherescape:SSM /ADT/backload/acl/tmp/*.CSV
 cp /ADT/backload/acl/tmp/*.CSV /ADT/archive/acl
-mv /ADT/backload/acl/tmp/*.CSV /ADT/data/acl
+for file in /ADT/backload/acl/tmp/*.CSV
+do
+  mv "$file" "${file%.CSV}.CSV.done"
+done
+mv /ADT/backload/acl/tmp/*.CSV.done /ADT/data/acl
