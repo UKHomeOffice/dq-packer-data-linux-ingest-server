2e8bb129f5c59ea0d585ffdb3d5516d0d1e97534
nick-otter <nicholas.a.otter@gmail.com>
2018-03-27 09:32:58 +0100

    Post Deploy: added .done in backload scripts

--- oag_backload_data_move.sh
+++ oag_backload_data_move.sh
@@ -3,4 +3,8 @@
 mv /ADT/backload/oag/*.xml /ADT/backload/oag/tmp
 chown wherescape:SSM /ADT/backload/oag/tmp/*.xml
 cp /ADT/backload/oag/tmp/*.xml /ADT/archive/oag
-mv /ADT/backload/oag/tmp/*.xml /ADT/data/oag
+for file in /ADT/backload/oag/tmp/*.xml
+do
+  mv "$file" "${file%.xml}.xml.done"
+done
+mv /ADT/backload/oag/tmp/*.xml.done /ADT/data/oag
