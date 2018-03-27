9689109602230d0ff399fcf78400e630bb9dde24
nick-otter <nicholas.a.otter@gmail.com>
2018-03-27 14:30:37 +0100

    Post Deploy: added edge case to backload scripts

--- oag_backload_data_move.sh
+++ oag_backload_data_move.sh
@@ -3,9 +3,9 @@
 mv /ADT/backload/oag/*.xml /ADT/backload/oag/tmp
 chown wherescape:SSM /ADT/backload/oag/tmp/*.xml
 cp /ADT/backload/oag/tmp/*.xml /ADT/archive/oag
-for file in /ADT/backload/oag/tmp/*.xml
-do
-  touch "${file%.xml}.xml.done"
+for file in /ADT/backload/oag/tmp/*.xml; do
+  [ -e "$file" ] || continue
+  touch "${file}.done"
 done
 mv /ADT/backload/oag/tmp/*.xml.done /ADT/data/oag
 mv /ADT/backload/oag/tmp/*.xml /ADT/data/oag
