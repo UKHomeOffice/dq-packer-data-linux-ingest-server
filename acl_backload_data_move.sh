3c2f518740d7be23b7bd2bc4e143caa0298c8dc1
nick-otter <nicholas.a.otter@gmail.com>
2018-03-27 14:30:37 +0100

    Post Deploy: added edge case to backload scripts

--- acl_backload_data_move.sh
+++ acl_backload_data_move.sh
@@ -3,9 +3,9 @@
 mv /ADT/backload/acl/*.CSV /ADT/backload/acl/tmp
 chown wherescape:SSM /ADT/backload/acl/tmp/*.CSV
 cp /ADT/backload/acl/tmp/*.CSV /ADT/archive/acl
-for file in /ADT/backload/acl/tmp/*.CSV
-do
-  touch "${file%.CSV}.CSV.done"
+for file in /ADT/backload/acl/tmp/*.CSV; do
+  [ -e "$file" ] || continue
+  touch "${file}.done"
 done
 mv /ADT/backload/acl/tmp/*.CSV.done /ADT/data/acl
 mv /ADT/backload/acl/tmp/*.CSV /ADT/data/acl
