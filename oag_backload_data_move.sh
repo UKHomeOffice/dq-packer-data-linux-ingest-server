bbb5ee93e8f890344547d9c47bc7a0c9ecb05f50
nick-otter <nicholas.a.otter@gmail.com>
2018-03-27 12:38:01 +0100

    Post Deploy: updated move to touch in backload scripts

--- oag_backload_data_move.sh
+++ oag_backload_data_move.sh
@@ -5,6 +5,7 @@ chown wherescape:SSM /ADT/backload/oag/tmp/*.xml
 cp /ADT/backload/oag/tmp/*.xml /ADT/archive/oag
 for file in /ADT/backload/oag/tmp/*.xml
 do
-  mv "$file" "${file%.xml}.xml.done"
+  touch "${file%.xml}.xml.done"
 done
 mv /ADT/backload/oag/tmp/*.xml.done /ADT/data/oag
+mv /ADT/backload/oag/tmp/*.xml /ADT/data/oag
