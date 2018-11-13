9395eb918bb3973f4116d87be2b9cb305e3f8801
Duncan Mountford <duncan9562@gmail.com>
2018-11-13 09:06:56 +0000

    Change tasks in playbook and remove copy step in packer.json

--- scripts/acl_oag_download.sh
+++ scripts/acl_oag_download.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Backups starting..."
 
-# Copy to S3 bucket
+# Copy from S3 bucket
 echo "Copying data directory to S3"
 /usr/local/bin/aws s3 cp --quiet s3://$BUCKET_NAME/data-linux-ingest-backups/acl/aclhistory.db /ADT/scripts/ || echo "FAILED!"
 /usr/local/bin/aws s3 cp --quiet s3://$BUCKET_NAME/data-linux-ingest-backups/acl/oabhistory.db /ADT/scripts/ || echo "FAILED!"
