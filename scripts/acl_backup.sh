f0854c766fc9a8b671aa48d11287b96ec5ff3134
Duncan Mountford <duncan9562@gmail.com>
2018-11-14 11:01:44 +0000

    Add Logrotate config, and amend AWS binary location

--- scripts/acl_backup.sh
+++ scripts/acl_backup.sh
@@ -7,6 +7,6 @@ echo "Backups starting..."
 
 # Copy to S3 bucket
 echo "Copying data directory to S3"
-/usr/local/bin/aws s3 cp --quiet $ACL_DB_LOCATION s3://$DATA_ARCHIVE_BUCKET_NAME/$ACL_S3_DESTINATION || echo "FAILED!"
+/usr/bin/aws s3 cp --quiet $ACL_DB_LOCATION s3://$DATA_ARCHIVE_BUCKET_NAME/$ACL_S3_DESTINATION || echo "FAILED!"
 
 exit
--- scripts/acl_oag_download.sh
+++ scripts/acl_oag_download.sh
@@ -7,7 +7,7 @@ echo "Backups starting..."
 
 # Copy from S3 bucket
 echo "Copying data directory to S3"
-/usr/local/bin/aws s3 cp --quiet s3://$DATA_ARCHIVE_BUCKET_NAME/$ACL_S3_DESTINATION /ADT/scripts/ || echo "FAILED!"
-/usr/local/bin/aws s3 cp --quiet s3://$DATA_ARCHIVE_BUCKET_NAME/$OAG_S3_DESTINATION /ADT/scripts/ || echo "FAILED!"
+/usr/bin/aws s3 cp --recursive --quiet s3://$DATA_ARCHIVE_BUCKET_NAME/$ACL_S3_DESTINATION /ADT/scripts/ || echo "FAILED!"
+/usr/bin/aws s3 cp --recursive --quiet s3://$DATA_ARCHIVE_BUCKET_NAME/$OAG_S3_DESTINATION /ADT/scripts/ || echo "FAILED!"
 
 exit
