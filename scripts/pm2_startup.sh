44cc4b6482253121e4af1dadaecd690a141deeea
david-wirelab <info@wirelab.co.uk>
2019-02-01 08:44:52 +0000

    remove NATS references

--- scripts/pm2_startup.sh
+++ scripts/pm2_startup.sh
@@ -5,7 +5,6 @@ set -e
 cd /appdata/data-transfer
 source ~/.virtualenvs/data-transfer/bin/activate
 export PYTHONPATH=.
-pm2 start -u wherescape ecosystem.config.js -- $DATA_LANDING_BUCKET_NAME $DATA_ARCHIVE_BUCKET_NAME $GA_BUCKET_NAME $GA_BUCKET_ACCESS_KEY_ID $GA_BUCKET_SECRET_ACCESS_KEY
-pm2 save
+pm2 start -u wherescape ecosystem.config.js -- $DATA_LANDING_BUCKET_NAME $DATA_ARCHIVE_BUCKET_NAME
 
 exit
