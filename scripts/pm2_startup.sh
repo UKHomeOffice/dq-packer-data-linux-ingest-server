9bfacb4c30a602dcd901fd2e81e5b98782ffa013
david-wirelab <info@wirelab.co.uk>
2019-03-05 15:07:04 +0000

    fix pm2 startup

--- scripts/pm2_startup.sh
+++ scripts/pm2_startup.sh
@@ -6,5 +6,6 @@ cd /appdata/data-transfer
 source ~/.virtualenvs/data-transfer/bin/activate
 export PYTHONPATH=.
 pm2 start -u wherescape ecosystem.config.js -- $DATA_LANDING_BUCKET_NAME $DATA_ARCHIVE_BUCKET_NAME
+pm2 save
 
 exit
